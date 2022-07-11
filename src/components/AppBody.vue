375d1da7e461ee258993ddad2814a17689ff5b0e
AngeloDalicandro <angelo.dalicandro1@gmail.com>
2022-07-11 17:36:01 +0200

    completed without bonus

--- src/components/AppBody.vue
+++ src/components/AppBody.vue
@@ -1,12 +1,15 @@
 <template>
 
     <main>
+        <SearchBar :genres="genres" @genreSelection="selectGenre" />
+
         <div class="container">
-            <BodyComponent v-for="album, index in albums" :key="index"
+            <BodyComponent v-for="album, index in filteredAlbums" :key="index"
             :img="album.poster"
             :name="album.title"
             :author="album.author"
             :year="album.year"
+            :genre="album.genre"
             />
         </div>
     </main>
@@ -16,20 +19,37 @@
 <script>
 import axios from 'axios';  
 import BodyComponent from './BodyComponent.vue'
+import SearchBar from './SearchBar.vue'
 
 export default {
     name: "AppBody",
     components: {
         BodyComponent,
+        SearchBar
     },
     data() {
         return {
             url: "https://flynn.boolean.careers/exercises/api/array/music",
-            albums: []
+            albums: [],
+            genres: ["All"],
+            selectedGenre: "All"
         }
     },
     computed: {
+        filteredAlbums() {
+            let albums = [];
+            if(this.selectedGenre === "All") {
+                albums = this.albums
+            } else {
+                            this.albums.forEach((album) => {
+                if(album.genre === this.selectedGenre) {
+                    albums.push(album);
+                }
+            })
+            }
 
+            return albums
+        }
     },
     created() {
         this.getAlbums();
@@ -38,10 +58,20 @@ export default {
         getAlbums() {
             axios.get(this.url).then((response) => {
                 this.albums = response.data.response;
+                this.getGenres();
             })
             .catch((err) => {
                 console.log(`error ${err}`);
             });
+        },
+        getGenres() {
+            for(let i = 0; i < this.albums.length; i++) {
+                if(!this.genres.includes(this.albums[i].genre))
+                this.genres.push(this.albums[i].genre);
+            }
+        },
+        selectGenre(genre) {
+            this.selectedGenre = genre;
         }
     }
 }
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="search-bar">
+        <div class="filter">
+            <label for="genre">Genere</label>
+
+            <select v-model="selectedGenre" @change="selectGenre(selectedGenre)" name="genre-selector" id="genre">
+                <option  v-for="genre, index in genres" :key="index" :value="genre">{{genre}}</option>
+            </select>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SearchBar",
+    data() {
+        return {
+            selectedGenre: "All",
+        }
+    },
+    props: {
+        genres: Array
+    },
+    methods: {
+        selectGenre(selection) {
+            this.$emit('genreSelection', selection);
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .search-bar {
+        margin-bottom: 2rem;
+        display: flex;
+        justify-content: center;
+
+        label {
+            color: white;
+            margin-right: 1rem;
+        }
+
+        select {
+            padding: 0.25rem 0.5rem;
+        }
+    }
+</style>
